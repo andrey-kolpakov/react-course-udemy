32a6b9598ffd6844cd5c4568600e8e329deb7888
Andrey Kolpakov <gfstels@gmail.com>
2023-05-01 01:18:12 +0600

    new

--- 20-fetch/src/App.js
+++ 20-fetch/src/App.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            
-        </div>
-    )
+    const [todo, setTodo] = useState(null)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/5')
+            .then((response) => response.json())
+            .then((json) => setTodo(json))
+    }, [])
+
+    console.log(todo)
+
+    return <div className="App">{todo && <h1> {todo.title} </h1>}</div>
 }
 
 export default App
