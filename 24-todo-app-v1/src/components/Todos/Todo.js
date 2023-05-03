72ef4f61f8b591d427ea648a30e2fbadd1f47627
Andrey Kolpakov <gfstels@gmail.com>
2023-05-03 12:35:34 +0600

    new

--- 24-todo-app-v1/src/components/Todos/Todo.js
+++ 24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,8 +1,18 @@
 import styles from './Todo.module.css'
-
+import { RiTodoFill } from 'react-icons/ri'
 
 function Todo({ currentItem, deleteTodo, index }) {
-    return <div className={styles.todo} onDoubleClick={() => {deleteTodo(index)}}>{currentItem}</div>
+    return (
+        <div
+            className={styles.todo}
+            onDoubleClick={() => {
+                deleteTodo(index)
+            }}
+        >
+            <RiTodoFill className={styles.todoIcon} />
+            <div>{currentItem}</div>
+        </div>
+    )
 }
 
 export default Todo
